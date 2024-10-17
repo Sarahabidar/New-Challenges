0d67db6554d3e6711927cabada1e0c3030a36f1f
Sara <sarah.abiidar@gmail.com>
2024-10-17 22:07:38 +0200

    use Link component to every pages

--- react-project/react-21/src/Pages/AboutUs/index.js
+++ react-project/react-21/src/Pages/AboutUs/index.js
@@ -1,7 +1,10 @@
+import { Link } from "react-router-dom";
+
 export default function AboutUs() {
   return (
     <div>
       <h1>About Us!</h1>
+      <Link to="/">To Home Page</Link>
     </div>
   );
 }
--- react-project/react-21/src/Pages/HomePage/index.js
+++ react-project/react-21/src/Pages/HomePage/index.js
@@ -1,7 +1,10 @@
+import { Link } from "react-router-dom";
+
 export default function HomePage() {
   return (
     <div>
       <h1>Home Page</h1>
+      <Link to="/about-us">To About US</Link>
     </div>
   );
 }
